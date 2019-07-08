23cc69debbc4245efb4daaebc765c9c545711643
alilinykh <aililnykh@dtcc.edu>
2019-07-08 12:51:31 -0400

    works

--- AddingMachine/evaluator.ts
+++ AddingMachine/evaluator.ts
@@ -1,3 +1,32 @@
-class Evaluator{
-    
+class Evaluator {
+    ops: string[];
+    vals: number[];
+
+    constructor() {}
+
+    run(toEvaluate: string) {
+        const tokens: string[] = toEvaluate.split('');
+        for (const token of tokens) {
+            if (token === '(') {// doNothing
+            } else if (token === '+') { this.ops.push(token);
+            } else if (token === '-') { this.ops.push(token);
+            } else if (token === '*') { this.ops.push(token);
+            } else if (token === '/') { this.ops.push(token);
+            } else if (token === 'sqrt') { this.ops.push(token);
+            } else if (token === ')') { this.ops.push(token);
+                                        const op: string  = this.ops.pop();
+                                        let v: number = this.vals.pop();
+                                        if (op === '+') {v = this.vals.pop() + v;
+                                        } else if (op === '-') {v = this.vals.pop() - v;
+                                        } else if (op === '*') {v = this.vals.pop() * v;
+                                        } else if (op === '/') {v = this.vals.pop() / v;
+                                        } else if (op === 'sqrt') { v = Math.sqrt(v);
+                                        }
+                                        this.vals.push(v);
+            } else {
+                this.vals.push(+token);
+            }
+        }
+        return this.vals.pop();
+    }
 }
--- AddingMachine/src/app/text-input/text-input.component.ts
+++ AddingMachine/src/app/text-input/text-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { templateJitUrl } from '@angular/compiler';
 
 @Component({
   selector: 'app-text-input',
@@ -6,8 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text-input.component.css']
 })
 export class TextInputComponent implements OnInit {
+  ops: string[] = [];
+  vals: number[] = [];
 
   constructor() { }
+
+  value: any = '';
+
+  onEnter(value: string) {this.value = this.run(value); }
+
+
+  run(toEvaluate: string) {
+    const tokens: string[] = toEvaluate.split(' ');
+    for (const token of tokens) {
+        if (token === '(') {// doNothing
+        } else if (token === '+') { this.ops.push(token);
+        } else if (token === '-') { this.ops.push(token);
+        } else if (token === '*') { this.ops.push(token);
+        } else if (token === '/') { this.ops.push(token);
+        } else if (token === 'sqrt') { this.ops.push(token);
+        } else if (token === ')') {
+
+                                    const op: string  = this.ops.pop();
+                                    let v: number = this.vals.pop();
+                                    if (op === '+') {v = this.vals.pop() + v;
+                                    } else if (op === '-') {v = this.vals.pop() - v;
+                                    } else if (op === '*') {v = this.vals.pop() * v;
+                                    } else if (op === '/') {v = this.vals.pop() / v;
+                                    } else if (op === 'sqrt') { v = Math.sqrt(v);
+                                    }
+                                    this.vals.push(v);
+        } else {
+            this.vals.push(Number.parseFloat(token));
+        }
+    }
+    return this.vals.pop();
+}
+
 
   ngOnInit() {
   }
